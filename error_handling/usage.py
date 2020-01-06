bb1b1988570d73e00607e153015cb33cd2ce7937
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2020-01-06 16:15:18 +0100

    Modified -h option

--- error_handling/usage.py
+++ error_handling/usage.py
@@ -6,16 +6,17 @@
 ## usage.py
 ##
 
+USAGE="""USAGE
+    ./105torus opt a0 a1 a2 a3 a4 n
+
+DESCRIPTION
+    opt       method option:
+                    1 for the bisection method
+                    2 for Newton's method
+                    3 for the secant method
+    a[0-4]    coefficients of the equation
+    n         precision (the application of the polynomial to the solution should
+                be smaller than 10^-n)"""
+
 def usage() :
-    usage="""USAGE
-        ./105torus opt a0 a1 a2 a3 a4 n
-    
-    DESCRIPTION
-        opt       method option:
-                      1 for the bisection method
-                      2 for Newton's method
-                      3 for the secant method
-        a[0-4]    coefficients of the equation
-        n         precision (the application of the polynomial to the solution should
-                  be smaller than 10^-n)"""
-    print(usage)
+    print(USAGE)
