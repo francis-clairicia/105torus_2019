33a80dab0355140d130fbea83ee79f18b909c221
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2020-01-08 19:13:17 +0100

    Begging of secant

--- methods/secant.py
+++ methods/secant.py
@@ -7,4 +7,14 @@
 ##
 
 def secant(coefficients, n) :
-    print("secant")
+    a0 = coefficients[0]
+    a1 = coefficients[1]
+    a2 = coefficients[2]
+    a3 = coefficients[3]
+    a4 = coefficients[4]
+    f = lambda x: (a4 * pow(x, 4)) + (a3 * pow(x, 3)) + (a2 * pow(x, 2)) + (a1 * x) + a0
+
+    x0 = 0
+    x1 = 1
+    while True:
+        x2 = x1 - (f(x1) - f(x0))
