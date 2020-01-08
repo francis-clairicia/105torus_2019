a88fe9d787ba25882fe5432e953f29f2f0b6de61
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2020-01-08 17:05:13 +0100

    Bisection method completed

--- methods/bisection.py
+++ methods/bisection.py
@@ -6,5 +6,26 @@
 ## bisection.py
 ##
 
-def bisection(coefficients, n) :
-    print("bisection")
+from math import pow
+
+def bisection(coefficients, n):
+    a0 = coefficients[0]
+    a1 = coefficients[1]
+    a2 = coefficients[2]
+    a3 = coefficients[3]
+    a4 = coefficients[4]
+    f = lambda x: (a4 * pow(x, 4)) + (a3 * pow(x, 3)) + (a2 * pow(x, 2)) + (a1 * x) + a0
+
+    a = 0
+    b = 1
+    p = pow(10, -n)
+    while (b - a) > p:
+        m = (a + b) / 2
+        if f(m) < 0:
+            a = m
+        else:
+            b = m
+        if (m * pow(10, n)) % 10 > 0:
+            print("x = {0:.6f}".format(round(m, n)))
+        else:
+            print("x = {0}".format(round(m, n)))
