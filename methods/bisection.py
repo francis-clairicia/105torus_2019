2a950d15a10d83db54af1b6ddf1096e0ef9b92f2
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-01-20 14:03:12 +0100

    Update: arguments given to functions: bisection, newton and secant are now directly split into 5 coefficients

--- methods/bisection.py
+++ methods/bisection.py
@@ -9,12 +9,7 @@
 import sys
 from math import pow
 
-def bisection(coefficients, n):
-    a0 = coefficients[0]
-    a1 = coefficients[1]
-    a2 = coefficients[2]
-    a3 = coefficients[3]
-    a4 = coefficients[4]
+def bisection(a0, a1, a2, a3, a4, n):
     f = lambda x: (a4 * pow(x, 4)) + (a3 * pow(x, 3)) + (a2 * pow(x, 2)) + (a1 * x) + a0
 
     a = 0
--- methods/secant.py
+++ methods/secant.py
@@ -9,12 +9,7 @@
 import sys
 from math import pow
 
-def secant(coefficients, n):
-    a0 = coefficients[0]
-    a1 = coefficients[1]
-    a2 = coefficients[2]
-    a3 = coefficients[3]
-    a4 = coefficients[4]
+def secant(a0, a1, a2, a3, a4, n):
     f = lambda x: (a4 * pow(x, 4)) + (a3 * pow(x, 3)) + (a2 * pow(x, 2)) + (a1 * x) + a0
 
     x0 = 0
