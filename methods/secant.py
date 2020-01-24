630f34024dad63a7e3120ae518307bdddf7cf9a1
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2020-01-24 10:47:43 +0100

    Changed place of return 84

--- methods/secant.py
+++ methods/secant.py
@@ -19,13 +19,13 @@ def secant(a0, a1, a2, a3, a4, n):
     while abs(x1 - x0) >= p:
         try:
             x2 = x1 - (f(x1) * (x1 - x0) / (f(x1) - f(x0)))
-            if (0 <= x2 <= 1) is False:
-                sys.exit(84)
         except ZeroDivisionError:
             sys.exit(84)
         solutions.append(x2)
         x0 = x1
         x1 = x2
+    if (0 <= x2 <= 1) is False:
+        sys.exit(84)
     for x in solutions:
         value = round(x, n)
         if value == x:
